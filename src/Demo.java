435b74602450d96a826b871f1dee64b54143d219
Matt Brown <mattbrown@spotify.com>
2025-09-25 22:30:47 -0400

    write to file as well as stdout

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 class Demo {
-  public static void main(String[] args) {
+
+  public static void main(String[] args) throws FileNotFoundException {
+
+    final Printer printer = new Printer(new PrintWriter("/tmp/out"));
+
     // add shutdown hook
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("Goodbye!")));
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      printer.println("Goodbye!");
+      printer.close();
+    }));
 
     for (int i = 0; i < 5; i++) {
       try {
@@ -9,7 +19,27 @@ class Demo {
       } catch (InterruptedException e) {
         break;
       }
-      System.out.println("Hello from Java");
+      printer.println("Hello from Java");
+    }
+  }
+
+
+  private static class Printer {
+
+    final PrintWriter writer;
+
+    private Printer(PrintWriter writer) {
+      this.writer = writer;
+    }
+
+    void close() {
+      writer.close();
+    }
+
+    void println(String message) {
+      writer.println(message);
+      writer.flush();
+      System.out.println(message);
     }
   }
 }
